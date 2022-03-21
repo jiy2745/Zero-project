3c201333be1255f37eb937b23f17c1ff5715ab54
jiy2745 <102042119+jiy2745@users.noreply.github.com>
2022-03-22 00:27:05 +0900

    Update Test.dart

--- Test.dart
+++ Test.dart
@@ -1,2 +1,15 @@
-##Hi
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Testapp',
+      home: MainPage(),
+    );
+  }
+}
   
