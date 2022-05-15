25620b895e04f9082bc96fc77736c4a318f43a18
jiy2745 <102042119+jiy2745@users.noreply.github.com>
2022-05-15 22:37:16 +0900

    Update get_lunch_information.dart

--- donghwa_school_meal/get_lunch_information.dart
+++ donghwa_school_meal/get_lunch_information.dart
@@ -3,11 +3,12 @@ import 'package:intl/intl.dart';
 import 'package:xml/xml.dart';
 import 'lunch_information_after_treatment.dart';
 
-const String myKey = ''; //openapi 개인 키 입력 (개인키 안넣으면 코드 실행 안됨!!!)
+const String myKey =
+    'a9e5f2d060964da1bdf013c17a32dd32'; //openapi 개인 키 입력 (개인키 안넣으면 코드 실행 안됨!!!)
 final String now = DateFormat('yyyyMMdd')
     .format(DateTime.now()); //오늘의 날짜를 yyyyMMdd형태로 저장 ex)20220502
 
-Future<List<String>> getSchoolMeal() async {
+Future<List<String>> getSchoolMeal(int lod) async {
   //급식 정보 가져오기
   final List<String> weekend = ['급식이 없습니다'];
   final Uri url = Uri.parse(
@@ -17,17 +18,28 @@ Future<List<String>> getSchoolMeal() async {
   final String rawLunchInformation = document
       .findAllElements('DDISH_NM')
       .toString(); //가져온 정보 중 급식 정보만 가져와서 스트링으로 번경
-  if (rawLunchInformation.length == 2) {
-    //주말, 방학 등의 이유로 급식 정보가 없을 시 '급식이 없습니다' 반환
-    return weekend;
+  if (lod == 0) {
+    if (rawLunchInformation.length == 2) {
+      //주말, 방학 등의 이유로 급식 정보가 없을 시 '급식이 없습니다' 반환
+      return weekend;
+    } else {
+      final List<String> lunchInformation =
+          afterTreatment(rawLunchInformation); //정보 후처리
+      return lunchInformation;
+    }
   } else {
-    final List<String> lunchInformation =
-        afterTreatment(rawLunchInformation); //정보 후처리
-    return lunchInformation;
+    if (rawLunchInformation.length == 2) {
+      //주말, 방학 등의 이유로 급식 정보가 없을 시 '급식이 없습니다' 반환
+      return weekend;
+    } else {
+      final List<String> dinnerInformation =
+          dinnerAfterTreatment(rawLunchInformation); //정보 후처리
+      return dinnerInformation;
+    }
   }
 }
 
-Future<String> getKcalInfo() async {
+Future<String> getKcalInfo(int lod) async {
   //킬로리 정보 가져오기, 위랑 똑같다
   String weekend = '0 Kcal';
   final Uri url = Uri.parse(
@@ -36,10 +48,20 @@ Future<String> getKcalInfo() async {
   final XmlDocument document = XmlDocument.parse(response.body);
   final String rawKcalInformation =
       document.findAllElements('CAL_INFO').toString();
-  if (rawKcalInformation.length == 2) {
-    return weekend;
+  if (lod == 0) {
+    if (rawKcalInformation.length == 2) {
+      return weekend;
+    } else {
+      final String kcalInformation = afterTreatmentKcal(rawKcalInformation);
+      return kcalInformation;
+    }
   } else {
-    final String kcalInformation = afterTreatmentKcal(rawKcalInformation);
-    return kcalInformation;
+    if (rawKcalInformation.length == 2) {
+      return weekend;
+    } else {
+      final String dinnerKcalInformation =
+          dinnerAfterTreatmentKcal(rawKcalInformation);
+      return dinnerKcalInformation;
+    }
   }
 }
